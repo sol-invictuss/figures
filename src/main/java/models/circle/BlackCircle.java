78c244d978e2d5a7668517ad5475eec83aaecc48
Roman-Siratsky <romadep228@gmail.com>
2020-10-15 15:19:42 +0300

    Added circles

---
+++ src/main/java/models/circle/BlackCircle.java
@@ -0,0 +1,9 @@
+package models.circle;
+
+public class BlackCircle extends Circle {
+
+    public void describe() {
+        System.out.println("CIRCLE: Black is not sad");
+    }
+
+}
---
+++ src/main/java/models/circle/WhiteCircle.java
@@ -0,0 +1,8 @@
+package models.circle;
+
+public class WhiteCircle extends Circle {
+    public void describe() {
+        System.out.println("CIRCLE: white as snow!");
+    }
+
+}
